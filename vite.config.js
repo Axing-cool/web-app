88f081965532e49054e4c61fb471664200d8007d
jiazexing <747253438@qq.com>
2023-04-10 11:14:17 +0800

    chore:配置不同环境打包

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({command,mode}) => {
+  const env = loadEnv(mode,process.cwd());
+  console.log(`env--->${mode}`);
+  const {VITE_APP_BUILD_BASE_URL} = env;
+  console.log(`VITE_APP_BUILD_BASE_URL:${VITE_APP_BUILD_BASE_URL}`);
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: VITE_APP_BUILD_BASE_URL
   }
 })
